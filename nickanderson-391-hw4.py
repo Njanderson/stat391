f4414e5b5fa1babdc9ea12e06116bece6adac85d
Nick Anderson <nja4@uw.edu>
2017-05-02 20:54:09 -0700

    Intermediate state - moving to ubuntu

--- nickanderson-391-hw4.py
+++ nickanderson-391-hw4.py
@@ -5,8 +5,9 @@
 
 import numpy as np
 import pandas as pd
+import math
 from sklearn import discriminant_analysis
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 
 # Weekly dataset
@@ -22,15 +23,42 @@ y = weekly_data.ix[:,-1]
 random_seed = 0
 np.random.seed(random_seed)
 
+def get_k_fold_ind(n, k, prop):
+    if len(prop)  != k - 1:
+        raise ValueError("Passed k and len(prop) disagree. len(prop) must equal k - 1.")
+    all_shuffled = np.random.permutation(n)
+    # Appending 1 will include the rest of the elements
+    ret = []
+    for p in np.append(prop, 1):
+        num_ele = math.floor(p * n)
+        ret.append(all_shuffled[:num_ele])
+        all_shuffled = np.delete(all_shuffled, np.arange(num_ele))
+
+    # Could also use np.split...
+    return ret
+
+
 def model_fit_score(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     return model.score(X_test, y_test)
+
+# Implementation of sklearn's model_selection's train_test_split
+def train_test_split(X, y, test_size):
+    n = len(X)
+    k = 2
+    prop = [test_size]
+    idx = get_k_fold_ind(n, k, prop)
+    train_idx = idx[0]
+    test_idx = idx[1]
+    return X.ix[train_idx], X.ix[test_idx], y.ix[train_idx], y.ix[test_idx]
 
 def test_models(X, y, repeat_x):
     scores = pd.DataFrame(columns=['LogReg', 'LDA', 'QDA'])
     for i in range(0, repeat_x):
         # Split into test and train: only use Lag1 and Lag2
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=i)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+        # print(X_train)
+        # print(y_train)
         model_lr = linear_model.LogisticRegression()
         lr_score = model_fit_score(model_lr, X_train, X_test, y_train, y_test)
 
@@ -44,6 +72,7 @@ def test_models(X, y, repeat_x):
         scores = scores.append(i_test_run, ignore_index=True)
     return scores
 
+
 repeat_x = 3
 print("\nTesting models with Lag1 and Lag2")
 print(test_models(X.ix[:, 1:3], y, repeat_x))
@@ -54,7 +83,7 @@ print(test_models(X.ix[:, 1:4], y, repeat_x))
 # Problem 2
 
 # Fixing random state for reproducibility
-np.random.seed(1)
-x = np.random.randn(100)
-eps = np.random.randn(100)
-y = x - 2*x**2 + eps
+# np.random.seed(0)
+# x = np.random.randn(100)
+# eps = np.random.randn(100)
+# y = x - 2*x**2 + eps
