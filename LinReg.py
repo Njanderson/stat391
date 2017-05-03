ef5abee802a49e68a4c387b88c1ba679de0438b0
Nick Anderson <nja4@uw.edu>
2017-05-03 00:07:39 -0700

    Need to compute LOOCV error

---
+++ LinReg.py
@@ -0,0 +1,90 @@
+# Nick Anderson
+# 1328354
+# nja4@uw.edu
+# Stat 391: HW 3
+import numpy as np
+
+# standardize accepts X_std, a numpy array of arrays,
+# returning a standardized version of the array, the column means,
+# and the column standard deviations
+def standardize(X_std):
+    col_means = np.mean(X_std, 0)
+    std = np.std(X_std, 0)
+    stdized = (X_std - col_means) / std
+    return (stdized, col_means, std)
+
+# add_ones accepts M, a numpy array of arrays,
+# returning a numpy array with a column of 1's pre-appended
+def add_ones(M):
+    # If a dimension needs to be added...
+    if len(M.shape) < 2:
+        M = np.reshape(M, (M.shape[0], 1))
+    return np.insert(M, 0, 1, axis=1)
+
+# standardize_design accepts design_mat, a numpy array,
+# returning a standardized version of the array with a
+# column of 1's pre-appended
+def standardize_design(design_mat):
+    stdized = standardize(design_mat)[0]
+    return add_ones(stdized)
+
+# compute_lsq_estimates returns the least squares coefficient estimates
+# for a linear regression using the predictors in design_mat with responses y.
+# Returns STANDARDIZED coefficients that CANNOT be used directly for predictions with unstandardized data
+# Returns flattened coefficients
+def compute_lsq_estimates(design_mat, y):
+    # β = (X^T X)^(−1) X^T y
+    # First standardize the predictors, adding a 1 column for the y-intercept, Beta_0
+    stdized_w_ones = standardize_design(design_mat)
+    tposed = np.transpose(stdized_w_ones)
+    inv_squared = np.linalg.inv(np.dot(tposed, stdized_w_ones))
+    stdized_coeff = np.ndarray.flatten(np.dot(np.dot(inv_squared, tposed), y))
+    return stdized_coeff
+
+# predict_lsq accepts X_test, an observation for which to find a prediction,
+# bar_X, the predictor column means, std_x, the predictor column standard deviations,
+# and returns a prediction using the linear regression model described
+# by hat_beta, with predictor mean bar_X and standard deviation std_X.
+def predict_lsq(X_test, bar_X, std_X, hat_beta):
+    # hat_beta now contains a flattened array of coefficients, but Beta_0 needs to be fit.
+    # Use y_bar = b_hat_0 +  b_hat_1 * x_bar_1 + ...
+    # b_hat_0 = y_bar - b_hat_1 * x_bar - .....
+    X_test_stdized = (X_test - bar_X) / std_X
+    return np.sum(X_test_stdized * hat_beta[1:4]) + hat_beta[0]
+
+# compute_std_err_lsq accepts design_mat, a numpy 2-d array of predictors,
+# y, the true value associated with the predictors, and hat_y, the predictions
+# for the predictors described by design_mat, returning a tuple, with
+# tuple[0] = rse, and tuple[1] = SE(Beta), a SE array
+def compute_std_err_lsq(design_mat, y, hat_y):
+    # RSE = sqrt((1/n-p-1)*RSS)
+    if len(design_mat.shape) < 2:
+        design_mat = np.reshape(design_mat, (design_mat.shape[0], 1))
+    n, p = float(design_mat.shape[0]), float(design_mat.shape[1])
+    rse = np.sqrt((1.0/(n-p-1.0))*np.sum(np.square(y - hat_y)))
+    # (X^T X)^(−1)
+    stdized_w_ones = standardize_design(design_mat)
+    tposed = np.transpose(stdized_w_ones)
+    inv_squared = np.linalg.inv(np.dot(tposed, stdized_w_ones))
+    return (rse, rse*np.sqrt(np.diagonal(inv_squared)))
+
+# compute_R2_lsq accepts y, the true response values, and hat_y,
+# the predicted response values, returning the R^2 statistic
+def compute_R2_lsq(y, hat_y):
+    # R^2 = 1 - RSS / TSS
+    RSS = np.sum(np.square(y - hat_y))
+    y_bar = np.mean(y)
+    TSS = np.sum(np.square(y - y_bar))
+    return 1.0 - RSS / TSS
+
+# compute_F_stat_lsq accepts y, the true response values, and hat_y,
+# the predicted response values, and p, the number of predictors,
+# returning the F_statistic
+def compute_F_stat_lsq(y, hat_y, p):
+    # F = ((TSS - RSS) / p) / (RSS / (n - p - 1))
+    n = float(y.shape[0])
+    RSS = np.sum(np.square(y - hat_y))
+    y_bar = np.mean(y)
+    TSS = np.sum(np.square(y - y_bar))
+    # print("Degrees of freedom for F-stat: " + str(n - p - 1))
+    return ((TSS - RSS) / p) / (RSS / (n - p - 1))
--- nickanderson-391-hw4.py
+++ nickanderson-391-hw4.py
@@ -3,12 +3,13 @@
 # nja4@uw.edu
 # Stat 391: HW 4
 
+from LinReg import *
 import numpy as np
 import pandas as pd
 import math
 from sklearn import discriminant_analysis
-# from sklearn.model_selection import train_test_split
 from sklearn import linear_model
+import matplotlib.pyplot as plt
 
 # Weekly dataset
 weekly_data_raw = pd.read_csv("Weekly.csv", usecols=range(1, 10))
@@ -20,7 +21,7 @@ y = weekly_data.ix[:,-1]
 # Problem 1
 
 # Reproducibility
-random_seed = 0
+random_seed = 1
 np.random.seed(random_seed)
 
 def get_k_fold_ind(n, k, prop):
@@ -42,7 +43,10 @@ def model_fit_score(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     return model.score(X_test, y_test)
 
-# Implementation of sklearn's model_selection's train_test_split
+# train_test_split splits predictors X and corresponding responses y
+# into a test and a training set for X and y, returning
+# X_train, X_test, y_train, y_test
+# Inspired by sklearn's model_selection's train_test_split
 def train_test_split(X, y, test_size):
     n = len(X)
     k = 2
@@ -55,10 +59,7 @@ def train_test_split(X, y, test_size):
 def test_models(X, y, repeat_x):
     scores = pd.DataFrame(columns=['LogReg', 'LDA', 'QDA'])
     for i in range(0, repeat_x):
-        # Split into test and train: only use Lag1 and Lag2
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
-        # print(X_train)
-        # print(y_train)
         model_lr = linear_model.LogisticRegression()
         lr_score = model_fit_score(model_lr, X_train, X_test, y_train, y_test)
 
@@ -72,10 +73,30 @@ def test_models(X, y, repeat_x):
         scores = scores.append(i_test_run, ignore_index=True)
     return scores
 
+# Problem 1a
+scores = pd.DataFrame(columns=['LogReg', 'LDA', 'QDA'])
+# X.ix[:, 1:3] required due to only using Lag1 and Lag2
+X_train, X_test, y_train, y_test = train_test_split(X.ix[:, 1:3], y, test_size=0.5)
+model_lr = linear_model.LogisticRegression()
+lr_score = model_fit_score(model_lr, X_train, X_test, y_train, y_test)
+
+model_lda = discriminant_analysis.LinearDiscriminantAnalysis()
+lda_score = model_fit_score(model_lda, X_train, X_test, y_train, y_test)
+
+model_qda = discriminant_analysis.QuadraticDiscriminantAnalysis()
+qda_score = model_fit_score(model_qda, X_train, X_test, y_train, y_test)
+
+i_test_run = pd.DataFrame([[lr_score, lda_score, qda_score]], columns=['LogReg', 'LDA', 'QDA'])
+scores = scores.append(i_test_run, ignore_index=True)
+print(scores)
 
 repeat_x = 3
+
+# Problem 1b
 print("\nTesting models with Lag1 and Lag2")
 print(test_models(X.ix[:, 1:3], y, repeat_x))
+
+# Problem 1c
 print("\nTesting models with Lag1, Lag2, and Lag3")
 print(test_models(X.ix[:, 1:4], y, repeat_x))
 
@@ -83,7 +104,64 @@ print(test_models(X.ix[:, 1:4], y, repeat_x))
 # Problem 2
 
 # Fixing random state for reproducibility
-# np.random.seed(0)
-# x = np.random.randn(100)
-# eps = np.random.randn(100)
-# y = x - 2*x**2 + eps
+x = np.random.randn(100)
+eps = np.random.randn(100)
+y = x - 2*x**2 + eps
+
+# plt.scatter(x, y)
+# plt.show()
+
+x = np.random.randn(100)
+eps = np.random.randn(100)
+y = x - 2*x**2 + eps
+
+x = np.reshape(x,(x.shape[0], 1))
+lin_score = 0
+quad_score = 0
+cubic_score = 0
+quartic_score = 0
+
+def get_model_err(x_train, x_test, y_train, y_test):
+    std = standardize(x_train)
+    print("Shapes:")
+    print(x_train.shape)
+    print(std[1].shape)
+    print(std[2].shape)
+    coef = compute_lsq_estimates(x_train, y_train)
+    pred = predict_lsq(x_test, std[1], std[2], coef)
+    return (y_test - pred)**2
+
+for i in range(0, len(x)):
+
+    # Delete the ith observations, which will form our x_train and y_train
+    x_train = np.delete(x, i)
+    y_train = np.delete(x, i)
+
+    # Get our test point
+    x_test = np.array(x[i])
+    y_test = y[i]
+
+    x_train = np.reshape(x_train,(x_train.shape[0], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], 1))
+    print(x_train.shape)
+    print(x_test.shape)
+
+    # Linear model
+    lin_score = lin_score + get_model_err(x_train, x_test, y_train, y_test)
+
+    # Quadratic model
+    x_test = np.append(x_test, x_test**2, 1)
+    quad = np.append(x_train, x_train**2, 1)
+    quad_score = quad_score + get_model_err(quad, x_test, y_train, y_test)
+
+    # Cubic model
+    x_test = np.append(x_test, x_test**3, 1)
+    cubic = np.append(quad, x_train**3, 1)
+    cubic_score = cubic_score + get_model_err(cubic, x_test, y_train, y_test)
+
+    # Quartic model
+    x_test = np.append(x_test, x_test**4, 1)
+    quartic = np.append(cubic, x_train**4, 1)
+    quartic_score = quartic_score + get_model_err(x_train, x_test, y_train, y_test)
+
+print((lin_score, quad_score, cubic_score, quartic_score))
